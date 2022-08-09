45cf6153fdbe08b47dc347bd20711ac3101a2055
Madi <madiasunik@gmail.com>
2022-08-09 22:58:33 +0600

    Module-3 - Key points_commit_#4

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -6,9 +6,13 @@ public class CopiedNames {
         int variable1 = 2000;
         int value = 1000;
         int variable2 = 4000;
-        int value1 = 3000;
+        int value2 = 3000;
         int variables = 5000;
-        int variables1 = 7000;
-        int value2 = 90000;
+        int variables2 = 7000;
+        int value3 = 90000;
+        int int1 = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myInteger2 = 1234567;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -1,15 +1,16 @@
 package lang.print.gaps.task2;
-
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_FIVE = 5;
+        final int VARIABLE_FOUR;
+        VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE;
+        VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
-        int VariableEight = 8;
+        int variableEight = 8;
     }
 }
